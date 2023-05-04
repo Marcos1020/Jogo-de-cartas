541021742d450d792cbc0e2a88c99a4af7e8fb3d
marcosvinicius <marcossanches710@gmail.com>
2023-05-04 20:53:59 -0300

    Criando a classe de ReturnIntegration

---
+++ src/main/java/com/sanches/jogodecartas/controller/response/ReturnIntegration.java
@@ -0,0 +1,23 @@
+package com.sanches.jogodecartas.controller.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class ReturnIntegration {
+
+    private boolean success;
+
+    @JsonProperty("deck_id")
+    private String deckId;
+
+    private Integer remaining;
+    private boolean shuffled;
+
+}
