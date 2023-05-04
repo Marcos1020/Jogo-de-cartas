a124927d7bc62b2d4b0cdc1e51abb27351f0bd43
marcosvinicius <marcossanches710@gmail.com>
2023-05-04 20:58:52 -0300

    Criando a a tabla  GameResponse, com possiveis alterações a serem executadas

---
+++ src/main/resources/DB_Migration/V1_vencedores.sql
@@ -0,0 +1,6 @@
+create table GameResponse(
+  ID_RODADA			numeric(100) not null,
+  VENCEDOR_DA_RODADA varchar(100) not null,
+  PONTUAÇÃO_VENCEDOR numeric(100) not null
+primary key (ID_RODADA));
+create sequence sq_tb_vencedores;
