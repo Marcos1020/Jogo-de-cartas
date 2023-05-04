ef218081d2a510434388e086f67a7dadaebd0515
marcosvinicius <marcossanches710@gmail.com>
2023-05-04 20:56:14 -0300

    Criando a interface CreateDeckIntegration, para integrar os sistemas e criar um deck novo

---
+++ src/main/java/com/sanches/jogodecartas/integrationGame/CreateDeckIntegration.java
@@ -0,0 +1,16 @@
+package com.sanches.jogodecartas.integrationGame;
+
+import com.sanches.jogodecartas.controller.response.ReturnIntegration;
+import com.sanches.jogodecartas.exception.BadRequestException;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "create-deck-game", url = "${feign.client.config.create-deck-game.url}")
+public interface CreateDeckIntegration {
+
+    @GetMapping(value = "/new/shuffle/", consumes = MediaType.APPLICATION_JSON_VALUE)
+    ReturnIntegration initializerGame(
+            @RequestParam("deck_count")final Integer deckCount)throws BadRequestException;
+}
