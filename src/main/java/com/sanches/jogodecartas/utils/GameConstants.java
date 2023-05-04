f8b2f322ee51f7ba0da71029f3493aef1338b37f
marcosvinicius <marcossanches710@gmail.com>
2023-05-04 20:52:13 -0300

    Criando a classe GameConstants

---
+++ src/main/java/com/sanches/jogodecartas/utils/GameConstants.java
@@ -0,0 +1,9 @@
+package com.sanches.jogodecartas.utils;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class GameConstants {
+
+    public final String SQ_TB_VENCEDORES = "sq_tb_vencedores";
+}
