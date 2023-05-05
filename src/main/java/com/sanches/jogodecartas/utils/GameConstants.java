4ee5d2eeae2681c393e5dab16ec716a23e7c2153
marcosvinicius <marcossanches710@gmail.com>
2023-05-05 14:52:21 -0300

    Adicionando mais Constants a classe para uma melhor pratica nos tratamneots de erros e não deixar muitos comentarios no código

--- src/main/java/com/sanches/jogodecartas/utils/GameConstants.java
+++ src/main/java/com/sanches/jogodecartas/utils/GameConstants.java
@@ -6,4 +6,8 @@ import lombok.experimental.UtilityClass;
 public class GameConstants {
 
     public final String SQ_TB_VENCEDORES = "sq_tb_vencedores";
+
+    public final String SQ_INITIALIZER_GAME = "sq_tb_initializer_game";
+
+    public final String ERROR_INITIALIZER_GAME = "O parametro deckCount não pode ser 0, favor insira um numero de baralhos para iniciar o jogo";
 }
