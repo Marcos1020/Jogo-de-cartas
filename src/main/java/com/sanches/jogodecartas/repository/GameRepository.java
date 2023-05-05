94f7a736a2e149a107692be8cf50524a8a4c48fa
marcosvinicius <marcossanches710@gmail.com>
2023-05-05 14:55:04 -0300

    Criando o repositório responsável por salvar os dados recebidos de ReturnIntegration  e salvar na EntityInitializerGame.java

--- src/main/java/com/sanches/jogodecartas/repository/GameRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sanches.jogodecartas.repository;
-
-import com.sanches.jogodecartas.entity.EntityGame;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameRepository extends JpaRepository<EntityGame, Long> {
-}
