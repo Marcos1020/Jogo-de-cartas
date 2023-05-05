2105f69972289032f5fa950a3b2b61a0ebd52774
marcosvinicius <marcossanches710@gmail.com>
2023-05-05 14:57:22 -0300

    Criando a logica correta na hora de salvar um novo deck e salvar, e também criando o método de GetAll

--- src/main/java/com/sanches/jogodecartas/service/GameService.java
+++ src/main/java/com/sanches/jogodecartas/service/GameService.java
@@ -2,40 +2,61 @@ package com.sanches.jogodecartas.service;
 
 import com.sanches.jogodecartas.controller.response.GameResponse;
 import com.sanches.jogodecartas.controller.response.ReturnIntegration;
+import com.sanches.jogodecartas.conversoes.Conversoes;
+import com.sanches.jogodecartas.entity.EntityInitializerGame;
 import com.sanches.jogodecartas.exception.BadRequestException;
 import com.sanches.jogodecartas.integrationGame.CreateDeckIntegration;
-import com.sanches.jogodecartas.repository.GameRepository;
+import com.sanches.jogodecartas.repository.GameInitializerRepository;
+import com.sanches.jogodecartas.repository.GameWinnerRepository;
+import com.sanches.jogodecartas.utils.GameConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
 @Component
 public class GameService {
 
-    private GameRepository gameRepository;
+    private final Conversoes conversoes;
+
+    private GameInitializerRepository initializerRepository;
+
+    private GameWinnerRepository gameRepository;
 
     private CreateDeckIntegration deckIntegration;
 
     @Autowired
-    public GameService(GameRepository gameRepository, CreateDeckIntegration deckIntegration){
+    public GameService(Conversoes conversoes,
+                       GameInitializerRepository initializerRepository,
+                       GameWinnerRepository gameRepository,
+                       CreateDeckIntegration deckIntegration){
+        this.conversoes = conversoes;
+        this.initializerRepository = initializerRepository;
         this.gameRepository = gameRepository;
         this.deckIntegration = deckIntegration;
     }
 
     public GameResponse initializerGame(final Integer deckCount)throws BadRequestException{
 
-        //Regra numero 1 Salavar os dados retornados
-        //criar um getAll que busca todos os IdsSalvos
+        Integer deckError = 0;
+        if (deckCount == deckError){
+            log.info(GameConstants.ERROR_INITIALIZER_GAME);
+            throw new BadRequestException(GameConstants.ERROR_INITIALIZER_GAME);
+        }
         ReturnIntegration returnIntegration = this.deckIntegration.initializerGame(deckCount);
 
-        //testando o retorno da integração
-        GameResponse gameResponse = GameResponse.builder().build();
-        gameResponse.setDeckId(returnIntegration.getDeckId());
-        log.info(returnIntegration.getDeckId());
+        EntityInitializerGame initializerGame = conversoes.convertReturnIntegrationToEntityInitializerGame(returnIntegration);
+        EntityInitializerGame initializerGameSave = this.initializerRepository.save(initializerGame);
 
+        GameResponse gameResponse = conversoes.convertEntityInitializerGameToGameResponse(initializerGameSave);
         return gameResponse;
+    }
+    public List<EntityInitializerGame> getAll() {
+
+        return initializerRepository.findAll();
     }
 }
