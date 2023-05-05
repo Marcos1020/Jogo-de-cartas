dac42a9809da125fad4a331937d20372608e19b4
marcosvinicius <marcossanches710@gmail.com>
2023-05-05 14:59:48 -0300

    Alterando o Script de criação do banco de dados VENCEDORESe adicionando o campo DT_REGISTER

--- src/main/resources/DB_Migration/V2_vencedores.sql
+++ src/main/resources/DB_Migration/V2_vencedores.sql
@@ -1,6 +1,7 @@
-create table GameResponse(
+create table TB_VENCEDORES(
   ID_RODADA			numeric(100) not null,
   VENCEDOR_DA_RODADA varchar(100) not null,
-  PONTUAÇÃO_VENCEDOR numeric(100) not null
+  PONTUAÇÃO_VENCEDOR numeric(100) not null,
+  DT_REGISTER  TIMESTAMP not null
 primary key (ID_RODADA));
 create sequence sq_tb_vencedores;
