88e9aff90271cc045304c1570cdd7f1af98ead9c
marcosvinicius <marcossanches710@gmail.com>
2023-05-05 14:55:59 -0300

    Alterando os campos da classe GameResponse.java para retornar de uma melhor forma no body

--- src/main/java/com/sanches/jogodecartas/controller/response/GameResponse.java
+++ src/main/java/com/sanches/jogodecartas/controller/response/GameResponse.java
@@ -5,14 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Builder
 public class GameResponse {
 
-    private Long idRodada;
-    private String vencedorDaRodada;
-    private Long pontuacaoVencedor;
+    private Long idGame;
+    private Integer remaining;
     private String deckId;
+    private Date dateRegister;
+
 }
