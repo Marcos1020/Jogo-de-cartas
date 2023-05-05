3edd452608999f755c4f5e7d492677a9ce31fb52
marcosvinicius <marcossanches710@gmail.com>
2023-05-05 14:51:19 -0300

    Alterando o nome da classe de EntityGame para EntityWinnerGame.java para uma melhor diferenciação das demais entidades, e adicionando o campo dateRegister

--- src/main/java/com/sanches/jogodecartas/entity/EntityWinnerGame.java
+++ src/main/java/com/sanches/jogodecartas/entity/EntityWinnerGame.java
@@ -1,5 +1,9 @@
 package com.sanches.jogodecartas.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.sanches.jogodecartas.utils.ConverterUtil;
+import com.sanches.jogodecartas.utils.DateAndTimeDeserializer;
 import com.sanches.jogodecartas.utils.GameConstants;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -7,6 +11,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
@@ -15,7 +20,7 @@ import javax.persistence.*;
 @Builder
 @Table(name = "TB_VENCEDORES")
 @SequenceGenerator(name = "sq_tb_vencedores", sequenceName = GameConstants.SQ_TB_VENCEDORES, allocationSize = 1)
-public class EntityGame {
+public class EntityWinnerGame {
 
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sq_tb_vencedores")
     @Id
@@ -27,4 +32,10 @@ public class EntityGame {
 
     @Column(name = "PONTUAÇÃO_VENCEDOR")
     private Long pontuacaoVencedor;
+
+    @JsonDeserialize(using = DateAndTimeDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = ConverterUtil.FORMATO_DATA)
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "DT_REGISTER")
+    private Date dateRegister;
 }
