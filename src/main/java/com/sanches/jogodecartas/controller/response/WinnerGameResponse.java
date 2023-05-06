4fd2abfe4a928c1d6946e6b0182e8f0b9d5fc1f4
marcosvinicius <marcossanches710@gmail.com>
2023-05-06 16:55:24 -0300

    Criando a classe WinnerGameResponse.java

---
+++ src/main/java/com/sanches/jogodecartas/controller/response/WinnerGameResponse.java
@@ -0,0 +1,23 @@
+package com.sanches.jogodecartas.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class WinnerGameResponse {
+
+    private Long idRodada;
+
+    private String vencedorDaRodada;
+
+    private Long pontuacaoVencedor;
+
+    private Date dateRegister;
+}
