5593ad29c4fb8fc067b210cd6ff1b39a546c5acf
marcosvinicius <marcossanches710@gmail.com>
2023-05-06 10:44:49 -0300

    Adicionando o inicio da logica do método que integra e retira as cartas e distribui entre os jogadores

--- src/main/java/com/sanches/jogodecartas/service/GameService.java
+++ src/main/java/com/sanches/jogodecartas/service/GameService.java
@@ -1,11 +1,13 @@
 package com.sanches.jogodecartas.service;
 
+import com.sanches.jogodecartas.controller.request.GameRequest;
 import com.sanches.jogodecartas.controller.response.GameResponse;
 import com.sanches.jogodecartas.controller.response.ReturnIntegration;
 import com.sanches.jogodecartas.conversoes.Conversoes;
 import com.sanches.jogodecartas.entity.EntityInitializerGame;
 import com.sanches.jogodecartas.exception.BadRequestException;
 import com.sanches.jogodecartas.integrationGame.CreateDeckIntegration;
+import com.sanches.jogodecartas.integrationGame.InitializerGameIntegration;
 import com.sanches.jogodecartas.repository.GameInitializerRepository;
 import com.sanches.jogodecartas.repository.GameWinnerRepository;
 import com.sanches.jogodecartas.utils.GameConstants;
@@ -22,22 +24,22 @@ import java.util.List;
 public class GameService {
 
     private final Conversoes conversoes;
-
     private GameInitializerRepository initializerRepository;
-
     private GameWinnerRepository gameRepository;
-
     private CreateDeckIntegration deckIntegration;
+    private InitializerGameIntegration gameIntegration;
 
     @Autowired
     public GameService(Conversoes conversoes,
                        GameInitializerRepository initializerRepository,
                        GameWinnerRepository gameRepository,
-                       CreateDeckIntegration deckIntegration){
+                       CreateDeckIntegration deckIntegration,
+                       InitializerGameIntegration gameIntegration){
         this.conversoes = conversoes;
         this.initializerRepository = initializerRepository;
         this.gameRepository = gameRepository;
         this.deckIntegration = deckIntegration;
+        this.gameIntegration = gameIntegration;
     }
 
     public GameResponse initializerGame(final Integer deckCount)throws BadRequestException{
@@ -47,7 +49,7 @@ public class GameService {
             log.info(GameConstants.ERROR_INITIALIZER_GAME);
             throw new BadRequestException(GameConstants.ERROR_INITIALIZER_GAME);
         }
-        ReturnIntegration returnIntegration = this.deckIntegration.initializerGame(deckCount);
+        ReturnIntegration returnIntegration = this.deckIntegration.initializerNewDeck(deckCount);
 
         EntityInitializerGame initializerGame = conversoes.convertReturnIntegrationToEntityInitializerGame(returnIntegration);
         EntityInitializerGame initializerGameSave = this.initializerRepository.save(initializerGame);
@@ -55,8 +57,19 @@ public class GameService {
         GameResponse gameResponse = conversoes.convertEntityInitializerGameToGameResponse(initializerGameSave);
         return gameResponse;
     }
+
     public List<EntityInitializerGame> getAll() {
 
         return initializerRepository.findAll();
+    }
+
+    public GameResponse gamePlay(GameRequest gameRequest)throws BadRequestException{
+
+        ReturnIntegration returnIntegration = this.gameIntegration.initializerGame(gameRequest.getDeckId(), gameRequest.getCountDraw());
+        GameResponse gameResponse = GameResponse.builder().build();
+        gameResponse.setRemaining(returnIntegration.getRemaining());
+        gameResponse.setDeckId(returnIntegration.getDeckId());
+
+        return gameResponse;
     }
 }
