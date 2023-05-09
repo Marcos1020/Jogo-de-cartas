4357e62bcf74442e9d3326c365b1b1b071cde81e
marcosvinicius <marcossanches710@gmail.com>
2023-05-08 22:13:01 -0300

    Alterando o nome do campo de countDrow para count

--- src/main/java/com/sanches/jogodecartas/integrationGame/InitializerGameIntegration.java
+++ src/main/java/com/sanches/jogodecartas/integrationGame/InitializerGameIntegration.java
@@ -3,7 +3,6 @@ package com.sanches.jogodecartas.integrationGame;
 import com.sanches.jogodecartas.controller.response.ReturnIntegration;
 import com.sanches.jogodecartas.exception.BadRequestException;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(value = "draw-card-initialazer-game", url = "${feign.client.config.draw-card-initialazer-game.url}")
 public interface InitializerGameIntegration {
 
-    @GetMapping(value = "/{deck_id}/draw/", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{deck_id}/draw/")
     ReturnIntegration initializerGame(
             @PathVariable("deck_id")final String deckId,
-            @RequestParam("count_draw") final Integer countDraw)throws BadRequestException;
+            @RequestParam("count") final Integer count)throws BadRequestException;
 }
