2eada22a76468d24f55de9f74128a4991d467d1a
marcosvinicius <marcossanches710@gmail.com>
2023-05-09 22:44:17 -0300

    Criando o construtor na classe de teste ErrorTests.java

--- src/test/java/com/sanches/jogodecartas/errors/ErrorTests.java
+++ src/test/java/com/sanches/jogodecartas/errors/ErrorTests.java
@@ -1,23 +1,34 @@
 package com.sanches.jogodecartas.errors;
 
 import com.sanches.jogodecartas.exception.BadRequestException;
+import com.sanches.jogodecartas.integrationgame.InitializerGameIntegration;
 import com.sanches.jogodecartas.service.GameService;
 import com.sanches.jogodecartas.utils.GameConstants;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.eq;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class ErrorTests {
 
     @InjectMocks
     private GameService gameService;
+
+    private InitializerGameIntegration gameIntegration;
+
+    @Autowired
+    public ErrorTests(InitializerGameIntegration gameIntegration) {
+        this.gameIntegration = gameIntegration;
+    }
 
     @Test
     public void shouldReturnErrorWhenTryingToGenerateANewDeck() {
@@ -33,9 +44,9 @@ public class ErrorTests {
     }
 
     @Test
-    public void mustTestANullReturn(){
+    public void mustTestANullReturn() {
 
-        BadRequestException exception = assertThrows(BadRequestException.class, () ->{
+        BadRequestException exception = assertThrows(BadRequestException.class, () -> {
             gameService.initializerGame(null);
         });
 
