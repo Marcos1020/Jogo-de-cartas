d0f1ac221a310d1fecd3f3c62ea60650ec8e882e
marcosvinicius <marcossanches710@gmail.com>
2023-05-09 22:43:29 -0300

    Refatorando o nome dos Campos

--- src/main/resources/dbmigration/V1_gameInitializer.sql
+++ src/main/resources/dbmigration/V1_gameInitializer.sql
@@ -2,6 +2,6 @@ create table TB_INITIALIZER_GAME(
   ID_GAME			numeric(100) not null,
   DECK_ID  varchar(100) not null,
   REMAINING numeric(100) not null,
-  DT_REGISTER  TIMESTAMP not null
+  DT_REGISTER  TIMESTAMP not null,
 primary key (ID_GAME));
 create sequence sq_tb_initializer_game;
